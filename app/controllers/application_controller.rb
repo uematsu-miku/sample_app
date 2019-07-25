4a945c991ae57b495621ad6e0346ead402810870
EC2 Default User <ec2-user@ip-172-31-47-38.ap-northeast-1.compute.internal>
2019-07-25 10:35:44 +0000

    Finish static pages,演習はまだ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  def hello
-    render html: "hello, world!"
-  end
+  #def hello
+  #  render html: "hello, world!"
+  #end
   
 end
